6c61976ec3c93731ed9180eba4550c278137691e
Yuya Kato <yuya.kato@divx.co.jp>
2023-06-25 13:33:01 +0900

    修正1

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.3'
 set :application, 'small_sub'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:yuya-ka/https://github.com/yuya-ka/small_sub.git'
+set :repo_url,  'git@github.com:yuya-ka/small_sub.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
